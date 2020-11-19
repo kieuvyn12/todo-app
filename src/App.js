0f02671182d64bdc7dd942e0905824b4e8ca82ac
KV Nguyen <kieuvyn12@gmail.com>
2020-11-18 21:49:52 -0500

    adds todos state in App and passes down to Form

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
 
   return (
     <div className="App">
       <header>
         <h1>KV's Todo List</h1>
       </header>
-      <Form setInputText={setInputText} />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
       <TodoList />
     </div>
   );
