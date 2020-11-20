00407bfca3f7321f4d1d1c4d973b6d26815a8f17
KV Nguyen <kieuvyn12@gmail.com>
2020-11-19 22:09:42 -0500

    adds filter functionality for dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Form from './components/Form';
@@ -7,6 +7,26 @@ import TodoList from './components/TodoList';
 function App() {
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+  useEffect(() => {
+    filterHandler()
+  }, [todos, status])
+
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => !todo.completed))
+        break;
+      default:
+        setFilteredTodos(todos)
+        break;
+    }
+  }
 
   return (
     <div className="App">
@@ -18,8 +38,9 @@ function App() {
         setTodos={setTodos}
         inputText={inputText}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} />
     </div>
   );
 }
